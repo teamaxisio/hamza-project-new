c63d895e1279275c6962bba444e3bbb425956f4a
Muhammad Areeb <159682548+teamaxisio@users.noreply.github.com>
2024-02-11 07:32:07 +0000

    new chanegs

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,15 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import FeedBacks from "./components/FeedBacks";
+import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Vite + React</h1>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://react.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      <FeedBacks feedbacks_arary={[1, 2, 3]} />
+    </h1>
   );
 };
 
